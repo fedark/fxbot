d719aad5f57e7d609014a11533073d9a0006b232
Fedar Kuzmich <fedar.kuzmich@xplanis.com>
2025-04-03 16:26:01 +0300

    Corrected history command configuration.

--- BotInfrastructure/Model/HistoryCommandConfiguration.cs
+++ BotInfrastructure/Model/HistoryCommandConfiguration.cs
@@ -4,5 +4,11 @@ public class HistoryCommandConfiguration
 {
     public required string Pattern { get; set; }
     public required string Prompt { get; set; }
-    public required (int FromMonthsAgo, string Title)[] HotIntervals { get; set; }
+    public required HistoryCommandHotIntervalConfiguration[] HotIntervals { get; set; }
+}
+
+public class HistoryCommandHotIntervalConfiguration
+{
+	public required int FromMonthsAgo { get; set; }
+	public required string Title { get; set; }
 }
